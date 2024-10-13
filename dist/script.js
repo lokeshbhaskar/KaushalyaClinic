47faf885428c940ecf3912b1d188cb9ae5e945be
lokeshbhaskar <lokeshbhaskar28@gmail.com>
2024-10-13 10:32:58 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,20 @@
+
+$(window).ready(function(){
+    //typing animation
+    new Typed('#type-it', {
+      strings: ['Care with Dr. Umesh Sharma, MBBS, MD'],
+      typeSpeed: 50,
+      loop:false,
+      showCursor:false
+    });
+  })
+
+$(window).ready(function(){
+    //typing animation
+    new Typed('#type-it1', {
+      strings: ['A Leading General Physician with Over 10 Years of Experience in Delivering Compassionate and Comprehensive Healthcare'],
+      typeSpeed: 10,
+      loop:false,
+      showCursor: false
+    });
+  })
